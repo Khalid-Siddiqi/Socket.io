b754065641b7d22889d052582a49c8136b096bc8
Khalid Khurshid Siddiqui <khalid20031016@gmail.com>
2024-09-10 09:43:34 +0500

    weered

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
-const WebSocketServer = require('ws').WebSocketServer;
-const si = require("systeminformation");
-const wss = new WebSocketServer({ port: 8080 });
+const app = require('express')();
+const server = require('http').Server(app);
+const port = 3000;
+const io = require('socket.io')(server);
 
-// wss.on("connection", function connection(ws) {
-//   ws.on("message", function message(data) {
-//     console.log("received: %s", data);
-//   });
-//   ws.send("something");
-// });
-
-wss.on("connection", function connection(ws) {
-  // Send an initial message when a client connects
-  ws.send("Connected to WebSocket server");
-
-  // Set up an interval to send the CPU load data every second
-  const intervalId = setInterval(async () => {
-    try {
-      const cpuTemp = JSON.stringify(await si.currentLoad());
-      ws.send(cpuTemp); // Send CPU load data to the WebSocket client
-    } catch (error) {
-      console.error('Error fetching CPU load:', error);
-    }
-  }, 1000);
-
-  // Handle the case when the client disconnects
-  ws.on("close", () => {
-    clearInterval(intervalId); // Clear the interval when the connection is closed
-    console.log("Client disconnected, stopping CPU load reporting.");
-  });
-
-  // Handle incoming messages from the client
-  ws.on("message", function message(data) {
-    console.log("received: %s", data);
-  });
+server.listen(port, ()=>{
+    console.log(`Server is listening at${port}`);
+    
 });
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/public/index.html')
+});
+
+io.on('connection',(socket)=>{
+    console.log('user connected');
+    socket.emit('message',{manny: 'hey how are you?'});
+    socket.on('another event',(data)=>{
+        console.log(data);
+        
+    })    
+})
